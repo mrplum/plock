78fdc6517db9d0cb7c5be71761e7ea2f2e0759c3
matias <mformento8@gmail.com>
2020-01-06 18:23:28 -0300

    change validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
-    validates :name, :repository, :cost, :start_at ,presence: true
-    validates :name, :repository, uniqueness: true
+    validates :name, :repository, :cost ,presence: true
+    validates :name, uniqueness: true
     validates :name, :repository, length: { minimum: 5 }
     validates :cost, length: { minimum: 1 }
 end
