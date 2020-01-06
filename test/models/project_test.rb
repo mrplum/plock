aa71e2bcf38aafee27a470db706ab6f9833c814b
matias <mformento8@gmail.com>
2020-01-06 18:23:39 -0300

    test running

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "create project" do
+    p  = Project.new(
+      name: "Is test",
+      repository: "github",
+      cost: 5,
+      start_at: Date.new);
+    assert p.valid?
+  end
+
+  test "name uniqueness" do
+    p  = Project.create(
+      name: "Is test",
+      repository: "github",
+      cost: 5,
+      start_at: Date.new);
+    p1 = Project.new(
+      name: "Is test",
+      repository: "github",
+      cost: 5,
+      start_at: Date.new);
+  assert !p1.valid?
+ end
 end
