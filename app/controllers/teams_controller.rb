6a46af9b999135cfb21bd1421d4735e55176b074
cuestalvaro <alvarosergiocuesta@gmail.com>
2020-01-06 15:42:27 -0300

    corrections in teams_controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -69,6 +69,6 @@ class TeamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params.fetch(:team, {})
+      params.require(:team).permit(:name)
     end
 end
