f77c33f4b0efb267a0602b4ae0343bf16820c7c6
cuestalvaro <alvarosergiocuesta@gmail.com>
2020-01-06 15:17:31 -0300

    change teams table

--- db/migrate/20200106173823_create_teams.rb
+++ db/migrate/20200106173823_create_teams.rb
@@ -2,8 +2,8 @@ class CreateTeams < ActiveRecord::Migration[6.0]
   def change
     create_table :teams do |t|
       t.string :name 
-      t.int :proyect_id
-      
+      t.integer :proyect_id
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_143801) do
+ActiveRecord::Schema.define(version: 2020_01_06_173823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_01_06_143801) do
     t.string "repository"
     t.decimal "cost"
     t.date "start_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.integer "proyect_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
