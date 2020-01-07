5a0e2d918806a2d7fcfc83567a6a18d19c026b3a
matias <mformento8@gmail.com>
2020-01-07 20:04:49 -0300

    Revert "WiP"
    This reverts commit 3f52769188df8875b40ffa78e3eb8441ea6a6691.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-u = User.create(name: "Usuario", lastname: "Seed", email: "mail@example.com", password: "123456")
-u1 = User.create(name:"Usuario1", lastname: "Seed1", email: "mail1@example.com", password: "123456")
-t = Team.create(name: "Plockers")
-
-Project.create(name: "Example", repository: "github", cost: 2, u)
