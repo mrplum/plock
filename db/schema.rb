4eda6a4775843a9bceec095071f1501bb71e2f18
Franco Brusatti <fbrusatti@gmail.com>
2020-01-07 10:33:07 -0300

    Reconciliate schema again

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_01_06_191356) do
-=======
 ActiveRecord::Schema.define(version: 2020_01_06_190353) do
 
->>>>>>> change validations
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
