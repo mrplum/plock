0bd316e95facf703cd43bfa62112dc6ff330d552
matias <mformento8@gmail.com>
2020-01-07 09:15:59 -0300

    tests added

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -22,6 +22,43 @@ class ProjectTest < ActiveSupport::TestCase
       repository: "github",
       cost: 5,
       start_at: Date.new);
-  assert !p1.valid?
- end
+    assert !p1.valid?
+  end
+
+  test "name not null" do 
+    p  = Project.create(
+      name: nil,
+      repository: "github",
+      cost: 5,
+      start_at: Date.new);
+    assert !p.valid?
+  end
+
+  test "repository not null" do 
+    p  = Project.create(
+      name: "Is test",
+      repository: nil,
+      cost: 5,
+      start_at: Date.new);
+    assert !p.valid?
+  end
+
+  test "cost not null" do 
+    p  = Project.create(
+      name: "Is test",
+      repository: "github",
+      cost: nil,
+      start_at: Date.new);
+    assert !p.valid?
+  end
+
+  test "length the name is minimum 5 characters" do 
+    p  = Project.create(
+      name: "test",
+      repository: "github",
+      cost: 5,
+      start_at: Date.new);
+    assert !p.valid?
+  end
+
 end
