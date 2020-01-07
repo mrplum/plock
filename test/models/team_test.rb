68348a7c1e9b701bc4977a2b4160c01bfd72e632
cuestalvaro <alvarosergiocuesta@gmail.com>
2020-01-07 15:30:01 -0300

    add test team

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -3,8 +3,17 @@ require 'test_helper'
 class TeamTest < ActiveSupport::TestCase
   
    test "create team" do
-    t  = Team.new(name: "team test")
+    t  = Team.create(name: "team test")
     assert t.valid?
   end
 
+  test "create team presence name" do
+    t  = Team.new
+    assert_not t.save
+  end
+
+  test "create team name minimun lenght 3" do
+    t  = Team.new(name: "te")
+    assert_not t.save
+  end
 end
