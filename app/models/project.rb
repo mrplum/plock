1afb4f7a6cff3e13572941b5fd396a714b050799
matias <mformento8@gmail.com>
2020-01-07 11:39:02 -0300

    asociation added

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,9 @@
 class Project < ApplicationRecord
-    validates :name, :repository, :cost, presence: true
+    belongs_to :users
+    has_many :tasks
+    has_many :teams
+
+    validates :name, :repository, :cost ,presence: true
     validates :name, uniqueness: true
     validates :name, :repository, length: { minimum: 5 }
 end
