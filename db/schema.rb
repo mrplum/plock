1f9cdd475c007336940d6f99f40316830a995d83
alejandrobarrios <barriosalejandro792@gmail.com>
2020-01-07 08:54:35 -0300

    test and new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_173823) do
+ActiveRecord::Schema.define(version: 2020_01_06_173959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_01_06_173823) do
     t.date "start_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
