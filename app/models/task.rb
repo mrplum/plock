ed3abe3c642ea2c35c7536d477c9eca3196efb8e
alejandrobarrios <barriosalejandro792@gmail.com>
2020-01-07 11:51:33 -0300

    add associations to Tasks model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   validates :name, :description, presence: true
+  belongs_to :project, dependent: :destroy
+  belongs_to :user, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,12 @@ ActiveRecord::Schema.define(version: 2020_01_06_191356) do
     t.datetime "starts_at"
     t.datetime "end_at"
     t.boolean "status", default: false
+    t.bigint "user_id", null: false
+    t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "teams", force: :cascade do |t|
