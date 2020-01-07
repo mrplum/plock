9a5ec72d9dfc484ce609c2b736bd5577510bb3d9
matias <mformento8@gmail.com>
2020-01-07 06:56:35 -0300

    delete validation unnecessarydelete validation unnecessary

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,4 @@ class Project < ApplicationRecord
     validates :name, :repository, :cost ,presence: true
     validates :name, uniqueness: true
     validates :name, :repository, length: { minimum: 5 }
-    validates :cost, length: { minimum: 1 }
 end
