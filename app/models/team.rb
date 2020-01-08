edf54c99a80e557104f363e717e549f9ec5ecb34
cuestalvaro <alvarosergiocuesta@gmail.com>
2020-01-08 11:10:35 -0300

    #20 rubocop team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
+# Team class
+#
 class Team < ApplicationRecord
-    has_many :team_users
-    has_many :users, through: :team_users
-    validates :name, presence: true, length: { minimum: 3 }
+  has_many :team_users
+  has_many :users, through: :team_users
+
+  validates :name, presence: true, length: { minimum: 3 }
 end
--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
+# TeamUser class
+#
 class TeamUser < ApplicationRecord
-    belongs_to :team 
-    belongs_to :user
-    validates :user_id, presence: true
-    validates :team_id, presence: true
+  belongs_to :team
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :team_id, presence: true
 end
