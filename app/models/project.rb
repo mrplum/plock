c2d41467e315e76a5dc130722170b53943ad9b64
matias <mformento8@gmail.com>
2020-01-08 00:56:30 -0300

    rubocop corrections

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,12 +3,12 @@
 # Project class
 #
 class Project < ApplicationRecord
-    belongs_to :user
-    has_one :team
-    has_many :tasks
-    has_many :users, through: :team
+  belongs_to :user
+  has_one :team
+  has_many :tasks
+  has_many :users, through: :team
 
-    validates :name, :repository, :cost, :user_id, presence: true
-    validates :name, uniqueness: true
-    validates :name, :repository, length: { minimum: 5 }
+  validates :name, :repository, :cost, :user_id, presence: true
+  validates :name, uniqueness: true
+  validates :name, :repository, length: { minimum: 5 }
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,88 +1,97 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
+# ProjectTest class
+#
 class ProjectTest < ActiveSupport::TestCase
-  
   def setup
     @user = users(:matias)
   end
 
   test 'create project' do
-    p  = Project.new(
+    p = Project.new(
       name: 'Is test',
       repository: 'github',
       cost: 5,
       start_at: Date.new,
-      user: @user)
+      user: @user
+    )
     p.save
     assert p.valid?
   end
 
   test 'name uniqueness' do
-    p  = Project.create(
+    Project.create(
       name: 'Is test',
       repository: 'github',
       cost: 5,
       start_at: Date.new,
-      user: @user)
+      user: @user
+    )
     p1 = Project.new(
       name: 'Is test',
       repository: 'github',
       cost: 5,
       start_at: Date.new,
-      user: @user)
+      user: @user
+    )
     p1.save
     assert_not p1.valid?
   end
 
-  test 'name not null' do 
-    p  = Project.create(
+  test 'name not null' do
+    p = Project.create(
       name: nil,
       repository: 'github',
       cost: 5,
       start_at: Date.new,
-      user: @user)
+      user: @user
+    )
     assert_not p.valid?
   end
 
-  test 'repository not null' do 
-    p  = Project.create(
+  test 'repository not null' do
+    p = Project.create(
       name: 'Is test',
       repository: nil,
       cost: 5,
       start_at: Date.new,
-      user: @user)
+      user: @user
+    )
     assert_not p.valid?
   end
 
-  test 'cost not null' do 
-    p  = Project.create(
+  test 'cost not null' do
+    p = Project.create(
       name: 'Is test',
       repository: 'github',
       cost: nil,
       start_at: Date.new,
-      user: @user)
+      user: @user
+    )
     assert_not p.valid?
   end
 
-  test 'user_id not null' do 
-    p  = Project.create(
+  test 'user_id not null' do
+    p = Project.create(
       name: 'Is test',
       repository: 'github',
       cost: 5,
       start_at: Date.new,
-      user: nil)
-    p.save
+      user: nil
+    )
     assert_not p.valid?
   end
 
-  test 'length the name is minimum 5 characters' do 
-    p  = Project.create(
+  test 'length the name is minimum 5 characters' do
+    p = Project.create(
       name: 'test',
       repository: 'github',
       cost: 5,
       start_at: Date.new,
-      user: @user)
+      user: @user
+    )
     assert_not p.valid?
   end
-
 end
