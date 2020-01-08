49e86f496bdabe86368e66d119f0451cf2e3022b
matias <mformento8@gmail.com>
2020-01-07 23:21:50 -0300

    Revert "WiP"
    This reverts commit 444baea7d0acea0baf725160931f3eb3508e7ea2.

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,107 +1,88 @@
-# frozen_string_literal: true
-
 require 'test_helper'
 
-# ProjectTest class
-#
 class ProjectTest < ActiveSupport::TestCase
+  
   def setup
     @user = users(:matias)
-    @team = teams(:one)
   end
 
-  test 'create project' do
-    p = Project.new(
-      name: 'Is test',
-      repository: 'github',
+  test "create project" do
+    p  = Project.new(
+      name: "Is test",
+      repository: "github",
       cost: 5,
       start_at: Date.new,
-      user: @user,
-      team: @team
-    )
+      user: @user)
     p.save
     assert p.valid?
   end
 
-  test 'name uniqueness' do
-    Project.create(
-      name: 'Is test',
-      repository: 'github',
+  test "name uniqueness" do
+    p  = Project.create(
+      name: "Is test",
+      repository: "github",
       cost: 5,
       start_at: Date.new,
-      user: @user,
-      team: @team
-    )
+      user: @user)
     p1 = Project.new(
-      name: 'Is test',
-      repository: 'github',
+      name: "Is test",
+      repository: "github",
       cost: 5,
       start_at: Date.new,
-      user: @user,
-      team: @team
-    )
+      user: @user)
     p1.save
     assert_not p1.valid?
   end
 
-  test 'name not null' do
-    p = Project.create(
+  test "name not null" do 
+    p  = Project.create(
       name: nil,
-      repository: 'github',
+      repository: "github",
       cost: 5,
       start_at: Date.new,
-      user: @user,
-      team: @team
-    )
+      user: @user)
     assert_not p.valid?
   end
 
-  test 'repository not null' do
-    p = Project.create(
-      name: 'Is test',
+  test "repository not null" do 
+    p  = Project.create(
+      name: "Is test",
       repository: nil,
       cost: 5,
       start_at: Date.new,
-      user: @user,
-      team: @team
-    )
+      user: @user)
     assert_not p.valid?
   end
 
-  test 'cost not null' do
-    p = Project.create(
-      name: 'Is test',
-      repository: 'github',
+  test "cost not null" do 
+    p  = Project.create(
+      name: "Is test",
+      repository: "github",
       cost: nil,
       start_at: Date.new,
-      user: @user,
-      team: @team
-    )
+      user: @user)
     assert_not p.valid?
   end
 
-  test 'user_id not null' do
-    p = Project.create(
-      name: 'Is test',
-      repository: 'github',
+  test "user_id not null" do 
+    p  = Project.create(
+      name: "Is test",
+      repository: "github",
       cost: 5,
       start_at: Date.new,
-      user: nil,
-      team: @team
-    )
+      user: nil)
     p.save
     assert_not p.valid?
   end
 
-  test 'length the name is minimum 5 characters' do
-    p = Project.create(
-      name: 'test',
-      repository: 'github',
+  test "length the name is minimum 5 characters" do 
+    p  = Project.create(
+      name: "test",
+      repository: "github",
       cost: 5,
       start_at: Date.new,
-      user: @user,
-      team: @team
-    )
+      user: @user)
     assert_not p.valid?
   end
+
 end
