cd3393ce1865e4e345ec5d3a0b7d2cb35da4ff9d
Álvaro Cuesta <49283536+cuestalvaro@users.noreply.github.com>
2020-01-09 15:02:56 -0300

    Team (#40)
    * #33 add user to the team
    
    * #34 add navbar

--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -5,6 +5,7 @@
 class TeamUser < ApplicationRecord
   belongs_to :team
   belongs_to :user
+
   validates :user_id, presence: true
-  validates :team_id, presence: true
+  # validates :team_id, presence: true
 end
