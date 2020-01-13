af37326a37cb70b159670aa7e43c7f50e1480b22
matias <mformento8@gmail.com>
2020-01-13 17:55:40 -0300

    template part javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -18,5 +17,27 @@ require("channels")
 
 import 'bootstrap' 
 import '../stylesheets/application'
-
-
+import '../../../vendor/javascript/jquery.min'
+import '../../../vendor/javascript/bootstrap.bundle.min'
+import '../../../vendor/javascript/fastclick'
+import '../../../vendor/javascript/nprogress'
+import '../../../vendor/javascript/Chart.min'
+import '../../../vendor/javascript/gauge.min'
+import '../../../vendor/javascript/bootstrap-progressbar.min'
+import '../../../vendor/javascript/icheck.min'
+import '../../../vendor/javascript/skycons'
+import '../../../vendor/javascript/jquery.flot'
+import '../../../vendor/javascript/jquery.flot.pie'
+import '../../../vendor/javascript/jquery.flot.time'
+import '../../../vendor/javascript/jquery.flot.stack'
+import '../../../vendor/javascript/jquery.flot.resize'
+import '../../../vendor/javascript/jquery.flot.orderBars'
+import '../../../vendor/javascript/jquery.flot.spline.min'
+import '../../../vendor/javascript/curvedLines'
+import '../../../vendor/javascript/date'
+import '../../../vendor/javascript/jquery.vmap'
+import '../../../vendor/javascript/jquery.vmap.world'
+import '../../../vendor/javascript/jquery.vmap.sampledata'
+import '../../../vendor/javascript/moment.min'
+import '../../../vendor/javascript/daterangepicker'
+import '../../../vendor/javascript/custom.min'
