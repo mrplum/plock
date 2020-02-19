7660547e50e6a04644c293a64355d6f0c252d0be
alejandrobarrios <barriosalejandro792@gmail.com>
2020-02-18 22:21:02 -0300

    add new file for elasticsearch

--- config/initializers/ElasticSearch.rb
+++ config/initializers/ElasticSearch.rb
@@ -1,25 +1,13 @@
 require 'elasticsearch/model'
 
-if Rails.env.development?
-	config = {
-  		host: "http://localhost:9200/",
-  		transport_options: {
-    	request: { timeout: 5 }
-  		}
+config = {
+  	host: "http://localhost:9200/",
+	transport_options: {
+	request: { timeout: 5 }
 	}
-	if File.exists?("config/elasticsearch.yml")
-  		config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].deep_symbolize_keys)
-	end
-else 
-	config = {
-  		host: "https://sxvkrp02t6:vvmms6q4rn@yew-522866683.us-east-1.bonsaisearch.net:443",
-  		transport_options: {
-    	request: { timeout: 5 }
-  		}
-	}
-	if File.exists?("config/elasticsearch.yml")
-  		config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].deep_symbolize_keys)
-	end
+}
+if File.exists?("config/elasticsearch.yml")
+	config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].deep_symbolize_keys)
 end
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
