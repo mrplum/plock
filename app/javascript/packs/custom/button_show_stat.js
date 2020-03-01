442b2541ae3ea97e9580da8b936c49316bb6e7b6
cuestalvaro <alvarosergiocuesta@gmail.com>
2020-03-01 17:49:21 -0300

    fix button_show_stat.js

--- app/javascript/packs/custom/button_show_stat.js
+++ app/javascript/packs/custom/button_show_stat.js
@@ -1,121 +1,38 @@
-if (document.getElementById('about-user-button') !== null) {
-  let buttonAboutUser = document.getElementById('about-user-button')
-  buttonAboutUser.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("about-user","project-user","statics-user","team-user")
-  });
-}
 
-if (document.getElementById('team-user-button') !== null) {
-  let buttonProjectUser = document.getElementById('team-user-button')
-  buttonProjectUser.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("team-user","project-user","statics-user","about-user")          
-  });
-}
-
-if (document.getElementById('project-user-button') !== null) {
-  let buttonProjectUser = document.getElementById('project-user-button')
-  buttonProjectUser.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("project-user","team-user","statics-user","about-user")          
-  });
-}
-
-if (document.getElementById('statics-user-button') !== null) {
-  let buttonStaticsUser = document.getElementById('statics-user-button')
-  buttonStaticsUser.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("statics-user","about-user","team-user","project-user")
-  });    
-}
-
-if (document.getElementById('about-project-button') !== null) {
-  let buttonAboutProject = document.getElementById('about-project-button')
-  buttonAboutProject.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("about-project","track-project","statics-project","")
-  });
-}
-
-if (document.getElementById('track-project-button') !== null) {
-  let buttonTrackProject = document.getElementById('track-project-button')
-  buttonTrackProject.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("track-project","statics-project","about-project","")          
-  }); 
-}
-
-if (document.getElementById('statics-project-button') !== null) {
-  let buttonStaticsProject = document.getElementById('statics-project-button')
-  buttonStaticsProject.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("statics-project","about-project","track-project","")
-  });  
-}
-
-if (document.getElementById('about-track-button') !== null) {
-  let buttonAboutTrack = document.getElementById('about-track-button')
-  buttonAboutTrack.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("about-track","track-interval","","")          
-  }); 
-}
-
-if (document.getElementById('track-interval-button') !== null) {
-  let buttonTrackInterval = document.getElementById('track-interval-button')
-  buttonTrackInterval.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("track-interval","about-track","","")
-  });
-}
-  
-if (document.getElementById('about-company-button') !== null) {
-  let buttonAboutCompany = document.getElementById('about-company-button')
-  buttonAboutCompany.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("about-company","company-user","company-project","team-company")
-  });
-}
-
-if (document.getElementById('company-user-button') !== null) {
-  let buttonCompanyUser = document.getElementById('company-user-button')
-  buttonCompanyUser.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("company-user","company-project","about-company","team-company")
-  });
-}
-
-if (document.getElementById('company-project-button') !== null) {
-  let buttonCompanyProject = document.getElementById('company-project-button')
-  buttonCompanyProject.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("company-project","about-company","company-user","team-company")
-  });
-} 
-
-if (document.getElementById('company-team-button') !== null) {
-  let buttonCompanyProject = document.getElementById('company-team-button')
-  buttonCompanyProject.addEventListener('click', function(event){
-    event.preventDefault();
-    startButton("team-company","company-project","about-company","company-user")
-  });
-} 
-  
-function startButton(name1,name2,name3,name4){
-  if (document.getElementById(name1).classList.contains('hidden') &&
-    !document.getElementById(name2).classList.contains('hidden')){
+startButton("about-user-button","about-user","project-user","statics-user","team-user")
+startButton("team-user-button","team-user","project-user","statics-user","about-user")
+startButton("project-user-button","project-user","team-user","statics-user","about-user")
+startButton("statics-user-button","statics-user","about-user","team-user","project-user")
+startButton("about-project-button","about-project","track-project","statics-project")
+startButton("track-project-button","track-project","statics-project","about-project")
+startButton("statics-project-button","statics-project","about-project","track-project")
+startButton("about-track-button","about-track","track-interval")
+startButton("track-interval-button","track-interval","about-track")
+startButton("about-company-button","about-company","company-user","company-project","team-company")
+startButton("company-user-button","company-user","company-project","about-company","team-company")
+startButton("company-project-button","company-project","about-company","company-user","team-company")
+startButton("company-team-button","team-company","company-project","about-company","company-user")
+ 
+function startButton(idDoc, name1, name2, name3 = "", name4 = ""){
+  if (document.getElementById(idDoc) !== null) {
+    let button = document.getElementById(idDoc)
+    button.addEventListener('click', function(event){
+      event.preventDefault();
+      if (document.getElementById(name1).classList.contains('hidden') &&
+        !document.getElementById(name2).classList.contains('hidden')){
           document.getElementById(name2).classList.add('hidden')
           document.getElementById(name1).classList.remove('hidden')
-  }  
-  else if (document.getElementById(name1).classList.contains('hidden') &&
-    !document.getElementById(name3).classList.contains('hidden')){
+      }  
+      else if (document.getElementById(name1).classList.contains('hidden') &&
+        !document.getElementById(name3).classList.contains('hidden')){
           document.getElementById(name3).classList.add('hidden')
           document.getElementById(name1).classList.remove('hidden')
-  }
-  else if (document.getElementById(name1).classList.contains('hidden') &&
-    !document.getElementById(name4).classList.contains('hidden')){
+      }
+      else if (document.getElementById(name1).classList.contains('hidden') &&
+        !document.getElementById(name4).classList.contains('hidden')){
           document.getElementById(name4).classList.add('hidden')
           document.getElementById(name1).classList.remove('hidden')
+      }
+    });
   }
 }
