aa38786402827d6b85d5e2addd2742ff68f88231
Matias Brusati <mformento8@gmail.com>
2020-07-22 20:04:24 -0300

    set env FOR elastic

--- config/initializers/ElasticSearch.rb
+++ config/initializers/ElasticSearch.rb
@@ -1,14 +1,14 @@
 
 config = {
-  	host: "http://localhost:9200/",
+  	host: ENV['ELASTICSEARCH_URL'],
 	transport_options: {
 		request: { 
 			timeout: 5 
 		}
 	}
 }
-if File.exists?("config/elasticsearch.yml")
-	config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].deep_symbolize_keys)
-end
+# if File.exists?("config/elasticsearch.yml")
+# 	config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].deep_symbolize_keys)
+# end
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
