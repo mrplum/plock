541eb4802bba315a7f631b31132dd25867b06ee7
cuestalvaro <alvarosergiocuesta@gmail.com>
2020-08-13 21:36:20 -0300

    add flash.js require

--- app/javascript/packs/custom/require.js
+++ app/javascript/packs/custom/require.js
@@ -2,6 +2,7 @@ document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
     require("packs/custom/home_index")
     require("packs/custom/datatables")
+    require("packs/custom/flash")
     require("packs/custom/chart-area")
     require("packs/custom/chart-bar")
     require("packs/custom/chart-pie")
