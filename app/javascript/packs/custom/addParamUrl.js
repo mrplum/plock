f0f23fcda8909b6eccbd9bd29d4bf6f03c493c7b
Matias Brusati <mformento8@gmail.com>
2020-09-06 17:15:42 -0300

    remove old query params

--- app/javascript/packs/custom/addParamUrl.js
+++ app/javascript/packs/custom/addParamUrl.js
@@ -1,6 +1,7 @@
 window.addParamUrlForm = function(button) {
   var url = document.getElementById('urlSendEmail')
   if (url != null) {
+    url.action = url.action.split('?')[0];
     url.action = url.action + `?type_report=${button.id}`
   }
 }
