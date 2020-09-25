0e0f360f19fb2c8ff46b807fd4ebdebb1d3eafb1
gamassimino <gamassimino01@gmail.com>
2020-09-25 17:02:32 -0300

    apply flex to track summary component

--- app/javascript/components/TracksSummary.js
+++ app/javascript/components/TracksSummary.js
@@ -3,22 +3,16 @@ import React from "react";
 const TracksSummary = (props) => {
 
   return (
-    <div className="row">
+    <div className="boxContainer">
 
       { props.summary.map( (item, index) => (
-        <div className="col-xl-3 col-md-6 mb-4" key={index}>
-          <div className={`card border-left-${item.color} shadow h-100 py-2`}>
-            <div className="card-body">
-              <div className="row no-gutters align-items-center">
-                <div className="col mr-2">
-                  <div className={`text-xs font-weight-bold text-${item.color} text-uppercase mb-1`}>{item.name}</div>
-                  <div className="h5 mb-0 font-weight-bold text-gray-800">{item.count}</div>
-                </div>
-                <div className="col-auto">
-                  <i className={`fas ${item.icon} fa-2x text-gray-300`}></i>
-                </div>
-              </div>
-            </div>
+        <div className={`boxItem ${item.color}`} key={index}>
+          <div>
+            <p className={`itemName ${item.color}`}>{item.name}</p>
+            <p className="itemCount">{item.count}</p>
+          </div>
+          <div>
+            <i className={`fas ${item.icon} fa-2x text-gray-300`}></i>
           </div>
         </div>
       )) }
